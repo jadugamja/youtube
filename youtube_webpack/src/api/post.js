4e384c09c0dc345b149274bbd5104709defe99ef
jasook <gheddong2@naver.com>
2023-11-01 23:39:09 +0900

    Add: get time difference function from posted date (data name is createdAt)

--- youtube_webpack/src/api/post.js
+++ youtube_webpack/src/api/post.js
@@ -18,3 +18,31 @@ export const selectAllVideo = async (page) => {
     }
 
 }
+
+export const getTimeAgo = (createdAt) => {
+    const currentDate = new Date();
+    const postedDate = new Date(createdAt);
+
+    const timeDifference = currentDate - postedDate;
+
+    const seconds = Math.floor(timeDifference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+
+    if (years > 0)
+        return `${years}년`
+    else if (months > 0)
+        return `${months}개월`
+    else if (days > 0)
+        return `${days}일`
+    else if (hours > 0)
+        return `${hours}시간`
+    else if (minutes > 0)
+        return `${minutes}분`
+    else if (seconds > 0)
+        return `${seconds}초`
+    
+}
--- youtube_webpack/src/components/Main/MainHomeItem.js
+++ youtube_webpack/src/components/Main/MainHomeItem.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
+
+import { getTimeAgo } from "../../api/post"
 import { VideoSection, ThumbnailContainerDiv, ThumbnailLink, ThumbnailImg, VideoDescContainerDiv, ChannelProfileImg, DescRightContainerDiv, DescRightHeaderDiv, DescRightProfileDiv, VideoTitle, KebabButton, NameSpan, VerifiedImg } from "./MainHomeItemStyle";
 
 const MainHomeItem = (props) => {
 
-    const {item: {nickname, profileImgPath, videoTitle, thumbnailImgPath, animatedThumbnail, hitsPerVideo, uploadedPeriod}, idx} = props;
+    // 서버에서 제공해주지 않은 데이터: videoTitle, animatedThumbnail, hitsPerVideo
+    const {item: {nickname, profileImgPath, videoTitle, thumbnailImgPath, animatedThumbnail, hitsPerVideo, createdAt}, idx} = props;
     const [isPlaying, setIsPlaying] = useState(false);
+
+    const uploadedPeriod = getTimeAgo(new Date(createdAt).getTime());
 
     // 마우스 오버 시
     const handleThumbnailMouseOver = () => setIsPlaying(true);
-
     // 마우스 떼면
     const handleThumbnailMouseOut = () => setIsPlaying(false);
 
@@ -27,7 +31,7 @@ const MainHomeItem = (props) => {
                 </div>
                 <DescRightContainerDiv>
                     <DescRightHeaderDiv row="between" col="baseline">
-                        <VideoTitle>{videoTitle}</VideoTitle>
+                        <VideoTitle>임시 영상 제목입니다.</VideoTitle>
                         <KebabButton></KebabButton>
                     </DescRightHeaderDiv>
                     <DescRightProfileDiv dir="col">
@@ -36,7 +40,7 @@ const MainHomeItem = (props) => {
                             <VerifiedImg hide />
                         </div>
                         <div>
-                            <NameSpan>{`조회수 ${hitsPerVideo}회 · ${uploadedPeriod} 전`}</NameSpan>
+                            <NameSpan>{`조회수 10만 회 · ${uploadedPeriod} 전`}</NameSpan>
                         </div>
                     </DescRightProfileDiv>
                 </DescRightContainerDiv>
